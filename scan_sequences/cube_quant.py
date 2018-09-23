a190fa9dfce97e3969603bf31fd49c6f2e29cf1f
Arjun Desai <arjunaashna@gmail.com>
2018-09-23 01:39:35 -0700

    added backbone for cones

--- scan_sequences/cube_quant.py
+++ scan_sequences/cube_quant.py
@@ -4,31 +4,28 @@ from scan_sequences.scans import NonTargetSequence
 from natsort import natsorted
 from nipype.interfaces.elastix import Registration, ApplyWarp
 from utils import io_utils
-from time import gmtime, strftime
 import scipy.ndimage as sni
 import numpy as np
 import file_constants as fc
 from utils import quant_vals as qv
 
 __EXPECTED_NUM_SPIN_LOCK_TIMES__ = 4
-
+__R_SQUARED_THRESHOLD__ = 0.9
+__INITIAL_P0_VALS__ = (1.0, 30.0)
 
 class CubeQuant(NonTargetSequence):
     NAME = 'cube_quant'
-    TEMP_PATH = os.path.join(fc.TEMP_FOLDER_PATH, '%s', '%s') % (NAME, strftime("%Y-%m-%d-%H-%M-%S", gmtime()))
 
-    def __init__(self, dicom_path=None, dicom_ext=None, save_dir=None, target_dict=None, interregistered_volumes_path=''):
+    def __init__(self, dicom_path=None, dicom_ext=None, save_dir=None, interregistered_volumes_path=''):
         super().__init__(dicom_path, dicom_ext)
         self.subvolumes = None
 
         if dicom_path is not None:
             self.save_dir = save_dir
 
-            self.subvolumes = self.__split_volumes__()
+            self.subvolumes = self.__split_volumes__(__EXPECTED_NUM_SPIN_LOCK_TIMES__)
             self.intermediate_save_dir = os.path.join(self.save_dir, self.NAME)
             self.intraregistered_data = self.__intraregister__(self.subvolumes)
-
-            self.interregister(target_dict['target_filepath'], target_dict['mask_filepath'])
 
         elif interregistered_volumes_path:
             self.subvolumes = self.__load_interregistered_files__(interregistered_volumes_path)
@@ -40,12 +37,13 @@ class CubeQuant(NonTargetSequence):
         base_spin_lock_time, base_image = self.intraregistered_data['BASE']
         files = self.intraregistered_data['FILES']
 
+        interregistered_dirpath = os.path.join(self.intermediate_save_dir, 'interregistered')
+
         # Register base image to the target image
         reg = Registration()
         reg.inputs.fixed_image = target
         reg.inputs.moving_image = base_image
         reg.inputs.output_path = os.path.join(self.intermediate_save_dir,
-                                              'interregistered',
                                               '%03d' % base_spin_lock_time)
         reg.inputs.parameters = [fc.ELASTIX_RIGID_PARAMS_FILE, fc.ELASTIX_AFFINE_PARAMS_FILE]
 
@@ -53,10 +51,6 @@ class CubeQuant(NonTargetSequence):
             reg.inputs.moving_mask = sni.gaussian_filter(np.asarray(mask, dtype=np.float32))
 
         transformation = reg.run()
-
-        interregistered_files = []
-        interregistered_files.append((base_spin_lock_time, base_image))
-        interregistered_dirpath = os.path.join(self.save_dir, 'interregistered')
 
         # Load the transformation file. Apply same transform to the remaining images
         for spin_lock_time, filename in files:
@@ -85,46 +79,16 @@ class CubeQuant(NonTargetSequence):
             svs.append(sv.reshape((1, -1)))
 
         vals = np.zeros(svs[0].shape)
+        r_squared = np.zeros(svs[0].shape)
         svs = np.concatenate(svs)
 
         for i in range(len(vals.shape[1])):
-            vals[i] = qv.fit_mono_exp(spin_lock_times, svs[..., i], p0=(1.0, 30.0))
+            vals[i], r_squared[i] = qv.fit_mono_exp(spin_lock_times, svs[..., i], p0=__INITIAL_P0_VALS__)
 
-        return vals.reshape(original_shape)
+        map_unfiltered = vals.reshape(original_shape)
+        r_squared = r_squared.reshape(original_shape)
 
-    def __split_volumes__(self):
-        refs_dicom = self.refs_dicom
-        volume = self.volume
-        subvolumes_dict = dict()
-
-        # get spin_lock_time time for each refs_dicom
-        for i in range(len(refs_dicom)):
-            echo_time = refs_dicom[i].EchoTime
-            if echo_time in subvolumes_dict.keys():
-                subvolumes_dict[echo_time].append(i)
-            else:
-                subvolumes_dict[echo_time] = [i]
-
-        # assert expected number of echos
-        num_spin_lock_times = len(list(subvolumes_dict.keys()))
-        assert num_spin_lock_times == __EXPECTED_NUM_SPIN_LOCK_TIMES__
-
-        # number of spin lock times should go evenly into subvolume
-        if (len(refs_dicom) % num_spin_lock_times) != 0:
-            raise ValueError('Uneven number of dicom files - %d dicoms, %d spin lock times' % (len(refs_dicom), num_spin_lock_times))
-
-        num_slices_per_subvolume = len(refs_dicom) / num_spin_lock_times
-
-        for key in subvolumes_dict.keys():
-            if (len(subvolumes_dict[key]) != num_slices_per_subvolume):
-                raise ValueError('spin_lock_time \'%s\' has %d slices. Expected %d slices' % (key, len(subvolumes_dict[key]), num_slices_per_subvolume))
-
-        for key in subvolumes_dict.keys():
-            sv = subvolumes_dict[key]
-            sv = volume[..., sv]
-            subvolumes_dict[key] = sv
-
-        return subvolumes_dict
+        return map_unfiltered * (r_squared > __R_SQUARED_THRESHOLD__)
 
     def __intraregister__(self, subvolumes):
         """
@@ -137,7 +101,7 @@ class CubeQuant(NonTargetSequence):
 
         # temporarily save subvolumes as nifti file
         ordered_spin_lock_times = natsorted(list(subvolumes.keys()))
-        raw_volumes_base_path = os.path.join(self.TEMP_PATH, 'raw')
+        raw_volumes_base_path = os.path.join(self.temp_path, 'raw')
 
         # Use first spin lock time as a basis for registration
         spin_lock_nii_files = []
@@ -160,7 +124,7 @@ class CubeQuant(NonTargetSequence):
             reg = Registration()
             reg.inputs.fixed_image = target_filepath
             reg.inputs.moving_image = spin_file
-            reg.inputs.output_path = io_utils.check_dir(os.path.join(self.TEMP_PATH,
+            reg.inputs.output_path = io_utils.check_dir(os.path.join(self.temp_path,
                                                                      'intraregistered',
                                                                      '%03d' % spin_lock_time))
             reg.inputs.parameters = [fc.ELASTIX_AFFINE_PARAMS_FILE]
@@ -170,29 +134,6 @@ class CubeQuant(NonTargetSequence):
 
         return {'BASE': (ordered_spin_lock_times[0], spin_lock_nii_files[0]),
                 'FILES': intraregistered_files}
-
-    def __load_interregistered_files__(self, interregistered_dirpath):
-        if 'interregistered' not in interregistered_dirpath:
-            raise ValueError('Invalid path for loading cube_quant interregistered files')
-
-        subdirs = io_utils.get_subdirs(interregistered_dirpath)
-        subdirs = natsorted(subdirs)
-
-        spin_lock_times = []
-        subvolumes = []
-        for subdir in subdirs:
-            spin_lock_times.append(float(subdir))
-            filepath = os.path.join(interregistered_dirpath, subdir, 'output.nii')
-            subvolume_arr = io_utils.load_nifti(filepath)
-            subvolumes.append(subvolume_arr)
-
-        assert len(spin_lock_times) == len(subvolumes), "Number of subvolumes mismatch"
-
-        subvolumes_dict = dict()
-        for i in range(len(spin_lock_times)):
-            subvolumes_dict[spin_lock_times[i]] = subvolumes[i]
-
-        return subvolumes_dict
 
     def load_data(self, load_dirpath):
         pass
--- scan_sequences/scans.py
+++ scan_sequences/scans.py
@@ -2,12 +2,16 @@ from abc import ABC, abstractmethod
 from utils import dicom_utils
 import numpy as np
 from utils import io_utils
-
+import os
+import file_constants as fc
+from time import gmtime, strftime
+from natsort import natsorted
 
 class ScanSequence(ABC):
     NAME = ''
 
     def __init__(self, dicom_path=None, dicom_ext=None):
+        self.temp_path = os.path.join(fc.TEMP_FOLDER_PATH, '%s', '%s') % (self.NAME, strftime("%Y-%m-%d-%H-%M-%S", gmtime()))
         self.tissues = []
         self.dicom_path = dicom_path
         self.dicom_ext = dicom_ext
@@ -77,6 +81,63 @@ class NonTargetSequence(ScanSequence):
         :return:
         """
         pass
+
+    def __split_volumes__(self, expected_num_echos):
+        refs_dicom = self.refs_dicom
+        volume = self.volume
+        subvolumes_dict = dict()
+
+        # get echo_time time for each refs_dicom
+        for i in range(len(refs_dicom)):
+            echo_time = refs_dicom[i].EchoTime
+            if echo_time in subvolumes_dict.keys():
+                subvolumes_dict[echo_time].append(i)
+            else:
+                subvolumes_dict[echo_time] = [i]
+
+        # assert expected number of echos
+        num_echo_times = len(list(subvolumes_dict.keys()))
+        assert num_echo_times == expected_num_echos
+
+        # number of spin lock times should go evenly into subvolume
+        if (len(refs_dicom) % num_echo_times) != 0:
+            raise ValueError('Uneven number of dicom files - %d dicoms, %d spin lock times/echos' % (len(refs_dicom), num_echo_times))
+
+        num_slices_per_subvolume = len(refs_dicom) / num_echo_times
+
+        for key in subvolumes_dict.keys():
+            if (len(subvolumes_dict[key]) != num_slices_per_subvolume):
+                raise ValueError('echo_time \'%s\' has %d slices. Expected %d slices' % (key, len(subvolumes_dict[key]), num_slices_per_subvolume))
+
+        for key in subvolumes_dict.keys():
+            sv = subvolumes_dict[key]
+            sv = volume[..., sv]
+            subvolumes_dict[key] = sv
+
+        return subvolumes_dict
+
+    def __load_interregistered_files__(self, interregistered_dirpath):
+        if 'interregistered' not in interregistered_dirpath:
+            raise ValueError('Invalid path for loading %s interregistered files' % self.NAME)
+
+        subdirs = io_utils.get_subdirs(interregistered_dirpath)
+        subdirs = natsorted(subdirs)
+
+        spin_lock_times = []
+        subvolumes = []
+        for subdir in subdirs:
+            spin_lock_times.append(float(subdir))
+            filepath = os.path.join(interregistered_dirpath, subdir, 'output.nii')
+            subvolume_arr = io_utils.load_nifti(filepath)
+            subvolumes.append(subvolume_arr)
+
+        assert len(spin_lock_times) == len(subvolumes), "Number of subvolumes mismatch"
+
+        subvolumes_dict = dict()
+        for i in range(len(spin_lock_times)):
+            subvolumes_dict[spin_lock_times[i]] = subvolumes[i]
+
+        return subvolumes_dict
 
 
 
