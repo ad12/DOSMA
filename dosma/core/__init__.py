415894f9839406c0bba6a0d31f7335510719e413
Arjun Desai <arjundd@stanford.edu>
2021-04-26 15:47:23 -0700

    fix refactor import

--- dosma/core/__init__.py
+++ dosma/core/__init__.py
@@ -1,11 +1,14 @@
 from dosma.core import (  # noqa: F401
+    io,
     med_volume,
     orientation,
 )
 
+from dosma.core.io import *
 from dosma.core.med_volume import *  # noqa
 from dosma.core.orientation import *  # noqa
 
 __all__ = []
+__all__.extend(io.__all__)
 __all__.extend(med_volume.__all__)
 __all__.extend(orientation.__all__)
