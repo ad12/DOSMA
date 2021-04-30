aedab356d27b7a772e31694cd11c4f072897ab08
Arjun Desai <arjundd@stanford.edu>
2021-04-30 09:13:04 -0700

    autoformat

--- tests/util.py
+++ tests/util.py
@@ -188,7 +188,9 @@ class ScanTest(unittest.TestCase):
             cls.dicom_dirpath = get_dicoms_path(
                 os.path.join(UNITTEST_SCANDATA_PATH, cls.SCAN_TYPE.NAME)
             )
-            cls.data_dirpath = get_data_path(os.path.join(UNITTEST_SCANDATA_PATH, cls.SCAN_TYPE.NAME))
+            cls.data_dirpath = get_data_path(
+                os.path.join(UNITTEST_SCANDATA_PATH, cls.SCAN_TYPE.NAME)
+            )
             io_utils.mkdirs(cls.data_dirpath)
 
     @classmethod
