380e74903f7f0c63ca0c8a14d40346c2344b9af5
Arjun Desai <arjundd@stanford.edu>
2021-05-12 22:06:34 -0700

    upgrade dosma to prerelease 0.0.13rc2

--- dosma/__init__.py
+++ dosma/__init__.py
@@ -7,4 +7,4 @@ from dosma.utils.env import debug  # noqa
 
 # This line will be programatically read/write by setup.py.
 # Leave them at the bottom of this file and don't touch them.
-__version__ = "0.0.13rc1"
+__version__ = "0.0.13rc2"
