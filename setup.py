cf7a9ff02207c267747eb15d0f273c4e6eea9fb7
Arjun Desai <arjundd@stanford.edu>
2022-04-17 21:38:02 -0700

    pin click version

--- setup.py
+++ setup.py
@@ -126,6 +126,7 @@ EXTRAS = {
         "flake8-comprehensions",
         "isort",
         "black==21.4b2",
+        "click==8.0.2",
         "simpleitk",
         "sphinx",
         "sphinxcontrib.bibtex",
