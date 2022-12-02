6309433b849c99c03d19e270c2d329f54607d506
Anthony Gatt <anthony@neuralseg.com>
2022-12-01 19:16:31 -0800

    Add bone seg model to models submodeuls namespace

--- dosma/models/__init__.py
+++ dosma/models/__init__.py
@@ -1,9 +1,11 @@
 from dosma.models import oaiunet2d, stanford_qdess, stanford_qdess_bone, util
 from dosma.models.oaiunet2d import *  # noqa
 from dosma.models.stanford_qdess import *  # noqa: F401, F403
+from dosma.models.stanford_qdess_bone import *  # noqa: F401, F403
 from dosma.models.util import *  # noqa
 
 __all__ = []
 __all__.extend(util.__all__)
 __all__.extend(oaiunet2d.__all__)
 __all__.extend(stanford_qdess.__all__)
+__all__.extend(stanford_qdess_bone.__all__)
